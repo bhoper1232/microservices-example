f6db3a9b63459a1c2351b143ed58cd033338e734
Andrey Solianyk <solianik123456@gmail.com>
2024-06-23 23:59:50 +0200

    Added Circuit Breaker

--- api-gateway/src/main/java/com/bhoper/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/bhoper/apigateway/ApiGatewayApplication.java
@@ -1,2 +1,11 @@
-package com.bhoper.apigateway;public class ApiGatewayApplication {
+package com.bhoper.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
--- order-service/src/main/java/com/bhoper/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/bhoper/order_service/controller/OrderController.java
@@ -2,13 +2,16 @@ package com.bhoper.order_service.controller;
 
 import com.bhoper.order_service.dto.OrderRequest;
 import com.bhoper.order_service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -18,10 +21,16 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<?> placeOrder(@RequestBody OrderRequest orderRequest) {
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body("Order placed successfully");
+        return CompletableFuture.supplyAsync(() ->"Order placed successfully\"");
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() ->"Oops! Something went wrong, please order after some time.");
     }
 
 }
